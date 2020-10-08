fe61280d11f4be5447cfa4e8eb63dab95a245803
eduardosgoncalves <eduardo.sgoncalves@hotmail.com>
2020-10-08 16:02:53 -0300

    Updates

---
+++ precache-manifest.a7b2db0124970a27c2e8455fe5f711c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e8b3476f43b4338dabe5935eb58aa2",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "28b8c477d840bbd0cb6d",
+    "url": "/simple-todo-app/static/css/main.65000c97.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b8c477d840bbd0cb6d",
+    "url": "/simple-todo-app/static/js/main.e5777862.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
